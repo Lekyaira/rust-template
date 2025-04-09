51fe384e3bdccce9d00668b0eac980638e626d2f
Ryan W. Anderson <anderson.ryan.w@live.com>
2025-04-08 19:16:24 -0500

    Updated the shell

--- shell.nix
+++ shell.nix
@@ -1,36 +1,26 @@
 let
   pkgs = import <nixpkgs> { };
   # Unstable Nix
+  # To use this, just prepend your package name with `unstable.`
   unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {};
   # Rust toolchain
   fenix = import (fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz") {};
-  rust_toolchain = fenix.complete.toolchain;
-  # rust_toolchain = fenix.combine [
-  #   fenix.complete.toolchain
-  #   fenix.targets.wasm32-unknown-unknown.latest.rust-std
-  # ];
+  rust_toolchain = fenix.combine [
+    fenix.stable.toolchain # Standard Rust
+    # fenix.complete.toolchain # Nightly
+    # fenix.targets.wasm32-unknown-unknown.latest.rust-std # Web Assembly Target
+  ];
   # Get project directory.
   pd = builtins.toString ./.;
 in
 pkgs.mkShell {
+  # C/C++ libraries go here.
   nativeBuildInputs = with pkgs; [
     rust_toolchain
-    pkg-config
   ];
 
+  # Other dependencies, cli tools, etc go here.
   buildInputs = with pkgs; [
-    at-spi2-atk
-    atkmm
-    cairo
-    gdk-pixbuf
-    glib
-    gtk3
-    harfbuzz
-    librsvg
-    libsoup_3
-    pango
-    webkitgtk_4_1
-    openssl
   ];
 
   # Cargo
@@ -38,10 +28,11 @@ pkgs.mkShell {
   CARGO_HOME = "${pd}/.cargo";
   CARGO_TARGET_DIR = "${pd}/.cargo/target";
   # Libraries
-  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.openssl pkgs.sqlite ];
+  # LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.openssl pkgs.sqlite ];
 
   shellHook = ''
-    #### Cargo ####
+#### Cargo ####
+# Make sure our Cargo directories exist.
     if [ ! -d $TMPDIR ]; then 
       mkdir -p $TMPDIR
     fi
